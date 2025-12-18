99a604d7c85a7af81c3501f8479ef262a8844818
killian bouhourd <killianbouhourd@gmail.com>
2025-12-18 09:59:24 +0100

    ajout script activité 1

--- Main.py
+++ Main.py
@@ -1,3 +1,21 @@
+import requests
+import pandas as pd
 import duckdb
+from datetime import datetime
+from pathlib import Path
 
-conn = duckdb.connect('test.db')
+
+#select des databases dans le dossier data
+db_files = [f for f in Path("data").glob("*.db")]
+for db_file in db_files:
+    con = duckdb.connect(db_file)
+    print(f"Base de données: {db_file}")
+    #différentes tables dans la base de données
+    df = con.execute("Show tables").fetchdf()
+    print(df)
+
+
+    df = con.execute("SELECT * FROM trafic").fetchdf()
+    print(df)
+    con.close() 
+
